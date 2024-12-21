58520fc2a34c5ef1dc6a41dca4e3e53695146082
Shivam Saxena <shivam2003official@gmail.com>
2024-12-21 11:16:57 +0530

    random

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import logo from "@/../public/logo.png";
 export default function Footer() {
 	return (
-		<div className="h-1/3 py-5 w-full relative flex flex-col items-center justify-center bg-[#18181b]">
+		<div className="h-1/3 py-5 w-full relative flex text-white flex-col items-center justify-center bg-[#18181b]">
             <div className="border-t border-b py-10 w-[90%] border-white">
                 <div className="inline-flex items-center justify-center w-full md:w-fit"><Image src={logo} className="size-14 lg:size-24 " alt="UNIONYX Logo"/> <span className="text-5xl lg:text-8xl bg-gradient-to-br from-slate-50 to-slate-500 tracking-wider bg-clip-text text-transparent font-bold font-montserrat">UNIONYX</span></div>
             </div>
--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -2,7 +2,7 @@ import { Globe } from "@/components/ui/world-map";
 
 export function GlobeDemo() {
 	return (
-		<div className="relative h-1/4 flex items-center justify-center px-40 pb-40 pt-0 md:pb-80">
+		<div className="relative h-1/4 flex items-start justify-center px-40 pb-52 pt-0 md:pb-80">
 			<Globe className="top-0 lg:scale-150" />
 		</div>
 	);
