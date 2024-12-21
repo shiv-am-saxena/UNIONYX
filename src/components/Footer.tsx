d1da59fac09eb11e9e1cf9dcae4b2c9f87cf8314
Shivam Saxena <shivam2003official@gmail.com>
2024-12-21 18:53:54 +0530

    Fixed responsiveness of footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,8 @@ import { IconBrandGithubFilled, IconBrandLinkedinFilled, IconBrandInstagramFille
 export default function Footer() {
 	return (
 		<div className="h-1/3 py-5 w-full relative flex text-white flex-col items-center justify-center bg-[#18181b]">
-			<div className="border-t border-b py-10 w-[90%] border-white flex justify-between flex-col lg:flex-row">
-				<div className="w-fit flex items-center justify-evenly flex-col">
+			<div className="border-t border-b py-10 w-[90%] border-white flex items-center justify-between gap-8 flex-col lg:flex-row">
+				<div className="w-fit flex items-center justify-evenly mx-auto lg:mx-0 flex-col">
 					<div className="inline-flex items-center justify-center w-full md:w-fit">
 						<Image src={logo} className="size-14 lg:size-24 " alt="UNIONYX Logo" />{" "}
 						<span className="text-5xl lg:text-8xl bg-gradient-to-br from-slate-50 to-slate-500 tracking-wider bg-clip-text text-transparent font-bold font-montserrat">UNIONYX</span>
@@ -26,20 +26,20 @@ export default function Footer() {
 						</a>
 					</div>
 				</div>
-                <div className="w-fit flex flex-col items-start gap-3">
-                    <h2 className="uppercase mb-5 font-semibold">Company</h2>
+                <div className="w-fit flex flex-col items-center lg:items-start gap-3">
+                    <h2 className="uppercase mb-5 font-semibold text-xl">Company</h2>
                     <a className="text-slate-100" href="/about" rel="noopener noreferrer">About Us</a>
                     <a className="text-slate-100" href="/support" rel="noopener noreferrer">Support</a>
                     <a className="text-slate-100" href="/privacy-policy" rel="noopener noreferrer">Privacy Policy</a>
                     <a className="text-slate-100" href="/terms-n-conditions" rel="noopener noreferrer">Terms and Conditions</a>
                     <a className="text-slate-100" href="/pricing-n-refs" rel="noopener noreferrer">Pricing and Refunds</a>
                 </div>
-                <div className="w-fit flex flex-col items-start gap-3">
-                    <h2 className="uppercase mb-5 font-semibold">Get In Touch</h2>
+                <div className="w-fit flex flex-col items-center lg:items-start gap-3">
+                    <h2 className="uppercase mb-5 font-semibold text-xl">Get In Touch</h2>
                     <a className="text-slate-100" href="tel:+916386665124" rel="noopener noreferrer">+91-638-666-5124</a>
                     <a className="text-slate-100" href="tel:+918604650124" rel="noopener noreferrer">+91-860-465-0124</a>
                     <a className="text-slate-100" href="mailto:secureshiv22@gmail.com" rel="noopener noreferrer">shivam2003official@gmail.com</a>
-                    <a className="text-slate-100" href="#" rel="noopener noreferrer">D-Block Indirapuram colony Sec-12,<br/> Lucknow(UP), 226001</a>
+                    <a className="text-slate-100 text-center lg:text-left" href="#" rel="noopener noreferrer">D-Block Indirapuram colony Sec-12,<br/> Lucknow(UP), 226001</a>
                 </div>
 			</div>
 			<div className="w-full px-4 flex items-center flex-col lg:flex-row justify-center md:px-16 pt-5">
